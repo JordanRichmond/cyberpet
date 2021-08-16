7df525a264f614ebc4757c99464480848c3c8eb7
JordanRichmond <richmond42@ymail.com>
2021-08-16 20:38:28 +0100

    Update

--- cyberpet.js
+++ cyberpet.js
@@ -1,2 +1,67 @@
-let input = prompt("input your details here:")
-console.log(input)
+const play = document.querySelector(".play");
+const feed = document.querySelector(".feed");
+const drink = document.querySelector(".drink");
+
+const happiness = document.querySelector(".happiness");
+const hunger = document.querySelector(".hunger");
+const thirst = document.querySelector(".thirst");
+
+class Animal {
+constructor(hunger = 50, thrist = 50, happiness = 50) {
+    (this.hunger = hunger),
+    (this.thrist = thirst),
+    (this.happiness = happiness);
+}
+
+play() {
+    this.happiness += 10;
+    this.hunger -= 9;
+    this.thirst -= 3;
+}
+
+feed() {
+    this.hunger += 5;
+    this.thirst -= 3;
+    this.happiness -= 3;
+}
+
+giveDrinks() {
+    this.thirst += 5;
+    this.hunger -= 3;
+    this.happiness += 3;
+}
+}
+
+const animal1 = new Animal();
+
+const updateData = function () {
+    happiness.childNodes[3].innerHTML = animal1.happiness;
+    hunger.childNodes[3].innerHTML = animal1.hunger;
+    thirst.childNodes[3].innerHTML = animal1.thirst;
+};
+
+const idle = function () {
+    animal1.happiness -= 1;
+    animal1.hunger -= 1;
+    animal1.thirst -= 1;
+    updateData();
+};
+
+play.addEventListener("click", function () {
+    animal1.play();
+    updateData();
+});
+
+feed.addEventListener("click", function () {
+    animal1.feed();
+    updateData();
+});
+
+drink.addEventListener("click", function () {
+    animal1.giveDrinks();
+    updateData();
+});
+
+setInterval(function () {
+    idle();
+}, 5000);
